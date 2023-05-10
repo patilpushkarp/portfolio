fda279c26c60878e658814669e0ff1b040c080d7
Pushkar Patil <patilpushkarp@gmail.com>
2023-05-10 17:50:32 +0530

    First commit

---
+++ portfolio/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app">
+    <NavigationBar></NavigationBar>
+  </div>
+</template>
+
+<script>
+import NavigationBar from './components/commons/NavigationBar.vue';
+
+export default{
+  components:{
+    NavigationBar
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Montserrat', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ portfolio/src/components/commons/NavigationBar.vue
@@ -0,0 +1,55 @@
+<template>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="../../assets/logo.png" alt="Logo" width="50" height="50">
+            </a>
+            <button class="navbar-toggler" type="button" @click="toggleMenu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" :class="{ 'show': isMenuOpen }">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+  
+<script>
+export default {
+    data() {
+        return {
+            isMenuOpen: false
+        };
+    },
+    methods: {
+        toggleMenu() {
+            this.isMenuOpen = !this.isMenuOpen;
+        }
+    }
+};
+</script>
+  
+<style scoped>
+.navbar-nav {
+    flex-direction: column;
+    height: 100%;
+}
+
+.nav-link {
+    padding: 0.5rem 1rem;
+}
+
+/* .collapse{
+    position: fixed;
+    background-color: black;
+} */
+
+</style>
+  
